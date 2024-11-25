35e20930e95fad72d29fdfe3681479b11ffd504a
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-11-25 20:28:35 +0700

    remove: Auth

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AuthPage() {
+  return (
+    <form>
+      <label>
+        Email
+        <input name="email" type="email" />
+      </label>
+      <label>
+        Password
+        <input name="password" type="password" />
+      </label>
+      <button>Sign In</button>
+    </form>
+  );
+}
+
+export default AuthPage;
--- src/types/db.ts
+++ src/types/db.ts
@@ -2,7 +2,8 @@ export type User = {
   id: string;
   email: string;
   password: string;
-  fullName: string | null;
+  emailVerified: Date | null;
+  name: string | null;
   image: string | null;
   createdAt: Date;
   updatedAt: Date;
@@ -13,15 +14,43 @@ export type Room = {
   title: string;
   description: string | null;
   userId: string;
-  createdAt: Date;
-  updatedAt: Date;
+  created_at: Date;
+  updated_at: Date;
 };
 
 export type Reservation = {
   id: string;
-  timePeriod: [Date, Date];
-  roomId: string;
-  userId: string;
+  timePeriod: string;
+  room_id: string;
+  user_id: string;
   createdAt: Date;
   updatedAt: Date;
+};
+
+export type VerificationToken = {
+  identifier: string;
+  expires: Date;
+  token: string;
+};
+
+export type Account = {
+  id: string;
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refresh_token: string | null;
+  access_token: string | null;
+  expires_at: number | null;
+  id_token: string | null;
+  scope: string | null;
+  session_state: string | null;
+  token_type: string | null;
+};
+
+export type Session = {
+  id: string;
+  userId: string;
+  expires: Date;
+  sessionToken: string;
 };
